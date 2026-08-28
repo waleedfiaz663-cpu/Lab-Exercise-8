910b38b9d1341c4c16320347d3f84200bcf25fa4
waleedfiaz663-cpu <waleedfiaz663@gmail.com>
2026-08-28 19:43:45 +0500

    Update ExerciseTest.java

--- ExerciseTest.java
+++ ExerciseTest.java
@@ -11,26 +11,35 @@ public class ExerciseTest {
         {
             System.out.println("passed");
         }
+            
         else
         {
             System.out.println("failed");
         }
     }
+    
      void displaydetails()
     {
         System.out.println("student details");
+        
         System.out.println("Name:"+Name);
+        
         System.out.println("marks:"+marks);
+        
         System.out.println("age:"+age);
     }
      
         public static void main(String[]args)
         {
             ExerciseTest s1=new ExerciseTest();
+            
             s1.Name="Waleed";
             s1.marks=77;
             s1.age=22;
+            
             s1.displaydetails();
             s1.checkresults();
-        }     
+            
+        }  
+    
     }
